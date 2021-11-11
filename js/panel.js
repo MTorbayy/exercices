fc83d5eb7ff576455c1abe1e26c7721704a4d3dc
Marie Torbay <marie.torbay@gmail.com>
2021-11-11 16:59:18 +0100

    menu interactif

--- js/panel.js
+++ js/panel.js
@@ -1,3 +1,19 @@
-class Panel {
+export class Panel {
+    constructor(title = "no title") {
+        const div = document.createElement('div'); //On crée un élément html div dans une constante div
+        this.html = div;
+        this.initialize(div, title);
+    }
 
+    initialize(div, title) {
+        //div.style = "background-image: url(images/" + img + ".jpg)"; //On définit une image en fond d'écran pour cette constante
+        div.style = "background-image: url(images/city-on-winter.jpg)";
+        div.className = "panel"; //on lui attribue la classe panel active
+        div.innerHTML = "<h3>" + title + "</h3>";
+    }
+
+    render() { //rendu. static : méthode de classe. Séparé du reste car processus lourd, pour laisser la possibilité de l'invoquer une seule fois sur l'ensemble des instances
+        document.querySelector(".container").appendChild(this.html); //Dans l'élément container on ajoute div. Il retrouve les mêmes mises en formes css correspondant à la classe panel active.
+    }
 }
+
--- script.js
+++ script.js
@@ -1,40 +1,22 @@
-//const panels = document.querySelectorAll(".container > *"); 
+import {Panel} from './js/panel.js';
 
-/*for (let panel of panels) {
-    
- });
-}*/
 
-const images = [
-    "city-on-winter",
-    "explore-the-world",
-    "mountains-and-clouds",
-    "sunny-beach",
-    "wild-forest"
+const panels = [
+    new Panel("city-on-winter"),
+    new Panel("explore-the-world"),
+    new Panel("mountains-and-clouds"),
+    new Panel("sunny-beach"),
+    new Panel("wild-forest")
 ];
 
-for (let image of images) {
-    const panel = createPanel(image);
-    panel.addEventListener('click', function() { 
-        //panels.forEach(p => p.classList.remove("active")); 
-        panel.classList.add("active"); 
-    });
+for (let panel of panels) {
+    panel.render();
 }
 
-function createPanel(img) {
-    const panelDiv = document.createElement('div'); //On crée un élément html div dans une constante panelDiv
-    //console.dir(panelDiv);
-    panelDiv.style = "background-image: url(images/" + img + ".jpg)"; //On définit une image en fond d'écran pour cette constante
-    panelDiv.className = "panel"; //on lui attribue la classe panel active
-    panelDiv.innerHTML = "<h3>Titre</h3>";
-    document.querySelector(".container").appendChild(panelDiv); //Dans l'élément container on ajoute panelDiv. Il retrouve les mêmes mises en formes css correspondant à la classe panel active.
-    return panelDiv;
-}
-
-
-/*
-<div style="background-image: url(images/explore-the-world.jpg)" class="panel active">
-<h3>Explore the World</h3>
-</div>
-*/
-
+//for (let image of images) {
+//    const panel = createPanel(image);
+//    panel.addEventListener('click', function() { 
+//        //panels.forEach(p => p.classList.remove("active")); 
+//        panel.classList.add("active"); 
+//    });
+//}
